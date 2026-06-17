e5ed578f5fc582c36f05f65613976b2523b77ea0
akki0312-tech <akkshay0312@gmail.com>
2026-06-17 13:02:56 +0530

    model Migrations completed

--- lms/accounts/apps.py
+++ lms/accounts/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class BorrowersConfig(AppConfig):
-    name = 'borrowers'
+class AccountsConfig(AppConfig):
+    name = 'accounts'
--- lms/loans/apps.py
+++ lms/loans/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
-    name = 'admin'
+class LoansConfig(AppConfig):
+    name = 'loans'
